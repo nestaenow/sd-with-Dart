e3c0eb18226464a464dd4bc839c58d7b741acb44
Nesta <nestaenow@gmail.com>
2021-08-03 03:41:30 +0200

    adjustments

--- input-output.dart
+++ input-output.dart
@@ -1,2 +1,13 @@
 import 'dart:io';
 
+main() {
+  //prompting the user to enter some data
+  stdout.writeln('What is your name: ?');
+
+  //declare a variable and store the value gotten from the user in it
+  String? name = stdin.readLineSync();
+
+  //print the value gotten
+  //that is the one stored in the variable name
+  print('My name is $name');
+}
