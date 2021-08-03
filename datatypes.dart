23c4a2e495f95ca7246713daef55b10a396712f7
Nesta <nestaenow@gmail.com>
2021-08-03 12:31:02 +0200

    adjustments

---
+++ datatypes.dart
@@ -0,0 +1,29 @@
+ main() {
+
+   //integer variable declaration
+   int amount1 = 100;
+   var amount2 = 200;
+
+   //print the values out
+   print('Amount1: $amount1 | Amount2: $amount2 \n');
+
+   //double variable declaration
+   double dAmount1 = 100.11;
+   var dAmount2 = 200.12;
+
+   print('Amount1: $dAmount1 | Amount2: $dAmount2 \n');
+
+   //String variable declaration
+   String name1 = 'Egbe';
+   var name2 = 'Nesta Enow';
+
+   print('My Name is: $name1 $name2 \n');
+
+   //Boolean variable delaration
+   bool isItTrue1 = true;
+   var isItTrue2 = false;
+
+   print('isItTrue1: $isItTrue11 | isItTrue2: $isItTrue2 \n');
+
+   //
+ }
