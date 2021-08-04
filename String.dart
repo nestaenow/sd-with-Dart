d6bee75f7cac239b94cb8e033f20964a2ef94c88
Nesta <nestaenow@gmail.com>
2021-08-04 03:54:20 +0200

    adjustments

---
+++ String.dart
@@ -0,0 +1,23 @@
+//Things to know about strings
+
+main(){
+  //possible ways to write strings
+  var s1 = 'This is the easiest way.';
+  var s2 = 'It\'s amazing how this thing works.';
+  var s3 = "Another way to do this is using double qoutes.";
+  var s4 = "It's as easy as that.";
+
+  //print output
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+  print(' ');
+
+  /*In order for the new line, tab, ... to not be implemented
+  while displaying strings Raw strings are being used */
+
+  //RAW String
+  var s = r'Wish I learned how to type in my early ages. \n Now I have to start its study all over.';
+  print(s);
+}
