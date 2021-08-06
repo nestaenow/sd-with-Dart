66ccd0f94515cd31431b56669f2524df3e6dfded
Nesta <nestaenow@gmail.com>
2021-08-06 18:30:24 +0200

    adjustments

--- type-conversions.dart
+++ type-conversions.dart
@@ -4,8 +4,8 @@ parse method of an interger*/
 //here's how it's done
 main(){
   //string to interger
-  var one = int.parse('1');
-  assert(one == 1);
+  var one = int.parse('2');
+  assert(one == 2);
 
   //same is done for an string to double conversion
   var onePointOne = double.parse('1.1');
