ae19d8a07b68bc281fd3b05d221356d5a5820636
Nesta <nestaenow@gmail.com>
2021-08-10 02:08:53 +0200

    adjustments

--- constants.dart
+++ constants.dart
@@ -1,3 +1,12 @@
 main(){
-  
+  const aConstNum = 1;
+  const aConstBool = false;
+  const aConstString = 'Nesta';
+
+  /* when using the const keyword/datatype keep in mind that
+  it takes the data type of the value which is being assigned to it */
+
+  print(aConstNum);
+  print(aConstBool);
+  print(aConstString);
 }
