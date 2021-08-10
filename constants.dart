b85f1d93bf316a24e2a6e0adc01fbe111910f0c6
Nesta <nestaenow@gmail.com>
2021-08-10 02:26:22 +0200

    adjustmwnts

--- constants.dart
+++ constants.dart
@@ -9,4 +9,20 @@ main(){
   print(aConstNum);
   print(aConstBool);
   print(aConstString);
+
+  /* to determine the constant type during 
+  runtime we use the .runtimetype as seen below */
+
+  print(aConstNum.runtimeType);
+  print(aConstBool.runtimeType);
+  print(aConstString.runtimeType);
+
+  /* If a variable is declared and no value is being assigned to it
+  note that is automatically assigned the value NULL */
+
+  //For example
+  var num;
+
+  //printing this variable verifies our hypothesis
+  print(num);
 }
