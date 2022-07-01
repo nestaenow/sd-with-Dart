542886e753f1272fc44c8d22d4502aac7f54f0f3
nestaenow <nestaenow@gmail.com>
2022-07-01 01:40:07 +0100

    Lists

--- functions.dart
+++ functions.dart
@@ -1,10 +1,20 @@
 void main() {
   List names = ['luca', 'lucy', 'lucio'];
-  //Adding new names to the List array/list
+  //The List datatype doesn't give a specific datatype to the list it creates but rather permits the adding of values of any datatype
+  
+  //This isn't good practice and to remedy this we use angle brackets to specify the list's datatype
+  //List<String> names = ['luca', 'lucy', 'lucio'];
+
+  //Adding new names to the List (Array in JavaScript)
   names.add('Clair');
+
+  //Adding an interger to the list
+  names.add(20);
 
   //Removing a name from the list
   names.remove('luca');
+
+  //Printing the names in the list
   print(names);
 
   //calling external fxns in main()
