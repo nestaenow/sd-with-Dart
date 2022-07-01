29042332c8688b2e40ebe368eb5d3429639c1311
nestaenow <nestaenow@gmail.com>
2022-07-01 02:31:33 +0100

    tried calling operator

--- operators.dart
+++ operators.dart
@@ -2,60 +2,62 @@ class numx {
   int Numx = 17;
 }
 
-main (){
-  int num = 20 + 11;
-  num = num - 2;
+main() {
+  numx a = numx();
+  print(a.Numx);
+  // int num = 20 + 11;
+  // num = num - 2;
 
-  print(num);
+  // print(num);
 
-  num = num % 5;
-  print(num);
+  // num = num % 5;
+  // print(num);
 
   //relational operators
-  if(num == 4){
-    print('true');
-  }
-  else{
-    print('false');
-  }
+  // if (num == 4) {
+  //   print('true');
+  // } else {
+  //   print('false');
+  // }
 
-  num *= 5;
-  print(num);
+  // num *= 5;
+  // print(num);
 
   //unary operator
-  ++num;
-  print(num);
-  num++;                    //which is same as num = num + ... a certain variable
-  print(num);
+  // ++num;
+  // print(num);
+  // num++; //which is same as num = num + ... a certain variable
+  // print(num);
 
   //logical operators
-  if(num > 2 && num < 30){                  //operatorfor logical AND – &&
-    print('Great!!');
-  }
-  else if(num > 2 || num == 0){             //operatorfor logical OR – ||
-    print('Too bad!');
-  }
-  if(num != 20){                            //operatorfor logical NOT – !
-    print('Finished successfully');
-  }
+  // if (num > 2 && num < 30) {
+  //   //operatorfor logical AND – &&
+  //   print('Great!!');
+  // } else if (num > 2 || num == 0) {
+  //   //operatorfor logical OR – ||
+  //   print('Too bad!');
+  // }
+  // if (num != 20) {
+  //   //operatorfor logical NOT – !
+  //   print('Finished successfully');
+  // }
 
   //Tenary operator
-  var x = 100;
-  var result = x % 2 == 0 ? 'Even' : 'Odd';
+  // var x = 100;
+  // var result = x % 2 == 0 ? 'Even' : 'Odd';
 
-  print(result);
+  // print(result);
 
   //Testing operator
-  if(x is int){
-    print('integer');
-  }
+  // if(x is int){
+  //   print('integer');
+  // }
 
   //Testing my numx class
-  var n = numx();
-  int number;
+  // var n = numx();
+  // int number;
 
-  number = n.Numx;
-  
-  print(number);
+  // number = n.Numx;
 
+  // print(number);
 }
