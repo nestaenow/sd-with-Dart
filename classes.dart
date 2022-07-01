c83bc04df837ffa80cb61f3d81d5a1ad810a9cbb
nestaenow <nestaenow@gmail.com>
2022-07-01 02:04:18 +0100

    progress

--- classes.dart
+++ classes.dart
@@ -1,3 +1,22 @@
 void main() {
-  
+  //Similarly to the manner in which variables are assigned datatypes like String, int, etc. a class is considered as a datatype. This is verified below:
+
+  User user1 =
+      User(); //User here is used as the datatype and also called as a fxn and stored in the variable user1
+
+  //Printing the attributes of the object
+  print(user1.userName);
+  print(user1.age);
+  user1.login();
+}
+
+//creating a class - User
+class User {
+  String userName = 'Nesta';
+  int age = 20;
+
+  //classes can contain fxns as seen below:
+  void login() {
+    print('User logged in!');
+  }
 }
