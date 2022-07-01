c0385c68c24b28a89ff46545b213a620e19c079c
nestaenow <nestaenow@gmail.com>
2022-07-01 01:24:34 +0100

    external fxns

---
+++ functions.dart
@@ -0,0 +1,10 @@
+void main() {
+  List names = ['luca', 'lucy', 'lucio'];
+  print(names);
+
+  //calling external fxns in main()
+  String name = lastName();
+  print(name);
+}
+
+String lastName() => 'Nesta'; //one way of w
